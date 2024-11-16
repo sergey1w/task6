f85840fa28b596cb7a0a740247e4fc753f5ae944
Сергей <26817252+sergey1w@users.noreply.github.com>
2024-11-16 13:58:37 +0300

    Initial Commit

---
+++ task6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  task6
+//
+//  Created by sergey on 16.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
